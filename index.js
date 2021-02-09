e43e8d36a85cbd8c153d1d0e4075dddf25cb3ccb
samleemcknight <samleemcknight@gmail.com>
2021-02-08 17:04:35 -0700

    initialized repo

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+app.get('/', function(req, res) {
+    res.send("success!")
+})
+
+app.get('/weather', function(req, res) {
+    res.send("success!")
+})
