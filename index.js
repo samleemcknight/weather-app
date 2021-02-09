e17fabeafde99d6bf1a22a336ff1a12f2d85a82c
samleemcknight <samleemcknight@gmail.com>
2021-02-09 11:10:32 -0700

    weather functional, but struggling with async

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express")
-const app = express()
-app.set('view engine', 'ejs')
+const ejsLayouts = require("express-ejs-layouts")
 
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
 const weatherJs = require("weather-js")
 
 app.get('/', function(req, res) {
     res.render('index')
 })
 
+// get request for form
+app.get('/weather', function(req, res) {
+    //get zipcode from form
+    let zipCode = req.query.zip
+    //put zipcode into url
+    res.redirect('weather/' + zipCode)
+})
+
 app.get('/weather/:zip', function(req, res) {
     let zipCode = req.params.zip;
-    res.render('weather.ejs', {
+    res.render('weather', {
         zip: zipCode,
         weather: weatherJs
     })
