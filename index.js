b440b598c689b3adf4b048c890d3afb01714c318
samleemcknight <samleemcknight@gmail.com>
2021-02-09 08:56:07 -0700

    got the weather calls working somewhat

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const app = express()
+app.set('view engine', 'ejs')
+
+const weatherJs = require("weather-js")
 
 app.get('/', function(req, res) {
-    res.send("success!")
+    res.render('index')
 })
 
-app.get('/weather', function(req, res) {
-    res.send("success!")
+app.get('/weather/:zip', function(req, res) {
+    let zipCode = req.params.zip;
+    res.render('weather.ejs', {
+        zip: zipCode,
+        weather: weatherJs
+    })
+    
 })
+
+app.listen(3000)
